ef88e56f96c8508b2cdc2d86ba171eb1e18ec3a8
SakethKanchi <sakethkanchi3@gmail.com>
2022-03-04 22:46:45 +0530

    build files

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ const App = () =>{
                 />
             </div>
             {
-                movies ?.length > 0  //If movies length > 0
+                movies?.length > 0  //If movies length > 0
                 ? (
                     <div className="container">
                         {movies.map((movie) => (
